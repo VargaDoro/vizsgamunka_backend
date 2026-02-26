214da5aa9d8d9f63826e1de3bea8640f5e6299fa
Eszti2222 <nagy.eszter2222@gmail.com>
2026-02-26 09:30:52 +0100

    bővítés Patient endpointok

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Document extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
     protected $fillable = [
         'patient_id',
         'doctor_id',
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,12 @@ use App\Http\Middleware\AdminMW;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\DocumentController;
+
+
 //autentikált végpontok
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/profile', [UserController::class, 'show_auth']);
@@ -25,3 +31,19 @@ Route::middleware(['auth:sanctum', AdminMW::class])->group(function () {
 //Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+/////PATIENT////
+
+// Bejelentkezett páciens saját adatai (PatientHomePage-nek)
+    Route::get('/patient/me', [PatientController::class, 'me']);
+
+    // Páciens időpontjai + foglalás
+    Route::get('/appointments/me', [AppointmentController::class, 'myAppointments']);
+    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::delete('/appointments/{appointment}', [AppointmentController::class, 'cancel']);
+
+    // Páciens dokumentumai
+    Route::get('/documents/me', [DocumentController::class, 'myDocuments']);
+    Route::get('/documents/{document}', [DocumentController::class, 'show']);
+    Route::get('/documents/{document}/download', [DocumentController::class, 'download']);
+
