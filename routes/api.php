f5042ed9966038654508e6a5bcdb9979349cab1e
VargaDoro <Varga.dorothea.timea@gmail.com>
2026-02-27 11:33:43 +0100

    doctor vegpontokk

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PrescriptionController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMW;
 use Illuminate\Http\Request;
@@ -23,3 +27,18 @@ Route::middleware(['auth:sanctum', AdminMW::class])->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{id}', [UserController::class, 'show']);
 });
+
+Route::middleware(['auth:sanctum', DoctorMW::class])
+    ->prefix('doctor')
+    ->group(function () {
+        // Saját páciensek listázása
+        Route::get('/patients', [PatientController::class, 'doctorIndex']);
+        // Egy páciens lekérdezése
+        Route::get('/patients/{patient_id}', [PatientController::class, 'doctorShow']);
+        // Dokumentum feltöltés
+        Route::post('/documents', [DocumentController::class, 'doctorStore']);
+        // Időpont CRUD
+        Route::get('appointments', [AppointmentController::class, 'doctorIndex']);//post,put,delete
+        // Recept CRUD
+        Route::apiResource('prescriptions', PrescriptionController::class);//get,post,put,delete külön doctor...
+    });
